f2662cc4e1ca34af892afd81f4434369a8c90d7a
OskarLehesNTIJohanneberg <oskar.lehes@elev.ga.ntig.se>
2023-03-29 09:59:01 +0200

    göra så att nav byter färg när man scrollar

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// var scrollTrigger = 60;
+
+// window.onscroll = function() {
+//   if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+//     document.getElementById("main-nav")[0].classList.add("inverted");
+//   } else {
+//     document.getElementById("main-nav")[0].classList.remove("inverted");
+//   }
+// };
+
+window.onscroll = function() {navFade()};
+
+var nav = document.getElementById("main-nav")
+
+var scroll = nav.offsetTop;
+
+function navFade() {
+    if (window.pageYOffset > 60) {
+      nav.classList.add("inverted");
+    } else {
+      nav.classList.remove("inverted");
+    }
+  }
