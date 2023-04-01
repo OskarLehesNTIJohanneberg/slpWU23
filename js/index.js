548bff9898e0282dd4dfd8bcf1a69fd2b7af63eb
OskarLehesNTIJohanneberg <oskar.lehes@elev.ga.ntig.se>
2023-04-01 23:12:46 +0200

    Animate hamburger meny and add "review" page

--- js/index.js
+++ js/index.js
@@ -1,18 +1,6 @@
-// var scrollTrigger = 60;
-
-// window.onscroll = function() {
-//   if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
-//     document.getElementById("main-nav")[0].classList.add("inverted");
-//   } else {
-//     document.getElementById("main-nav")[0].classList.remove("inverted");
-//   }
-// };
-
 window.onscroll = function() {navFade()};
 
 var nav = document.getElementById("main-nav")
-
-var scroll = nav.offsetTop;
 
 function navFade() {
     if (window.pageYOffset > 60) {
@@ -22,3 +10,36 @@ function navFade() {
     }
   }
 
+function changeClass(e) {
+  if (e.target.className.trim() === 'work-boxes') {
+   e.target.className = 'work-boxes-pressed';
+} else {
+   e.target.className = 'work-boxes';
+}
+}
+// Hämta hambuger meny elementet
+document.getElementById('checkbox').addEventListener('click', ActiveClasses)
+
+//  Hämta alla länkar från menyn och kolla om man klickar på dem, sen ändra classerna om man gör det 
+document.querySelectorAll('.nav-link').forEach(item => {
+  item.addEventListener('click', ActiveClasses)
+})
+
+//  Simple function som ändar alla olika linjer till olika classer 
+function ActiveClasses() {
+  const allElements = document.querySelectorAll('.dynamic')
+  allElements.forEach(item => {
+    if (item.classList.contains("menu-items")) {
+      item.classList.toggle("activemenu")
+    }
+    if (item.classList.contains("line1")) {
+      item.classList.toggle("active1")
+    }
+    if (item.classList.contains("line2")) {
+      item.classList.toggle("active2")
+    }
+    if (item.classList.contains("line3")) {
+      item.classList.toggle("active3")
+    }
+  })
+}
